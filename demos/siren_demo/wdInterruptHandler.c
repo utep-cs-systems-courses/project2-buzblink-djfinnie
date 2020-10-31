5822b3725acf94bee3fdc3627dcd8af89208024c
djfinnie <djfinnie.miners.utep.edu>
2020-10-31 15:43:42 -0600

    Put all necessary files together in the the same directory

---
+++ demos/siren_demo/wdInterruptHandler.c
@@ -0,0 +1,21 @@
+#include <msp430.h>
+#include "stateMachines.h"
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static char blink_count = 0;
+  static int count = 0;
+
+  if ((++count %25) == 0) buzzer_advance();
+
+  if (count == 250) {
+    main_state_advance();
+    count = 0;
+  }
+  
+  if (++blink_count == 125) {
+    buzzer_advance();
+    state_advance();
+    blink_count = 0;
+  }
+}
